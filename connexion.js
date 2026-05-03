1916286ed45bbc95e9293130362831ddc81a9843
Tomas <felicianoalmeidatomas@gmail.com>
2026-05-03 16:51:06 +0200

    Create connexion.js

---
+++ connexion.js
@@ -0,0 +1,9 @@
+let name=prompt("Quel est votre nom?")
+let password=prompt("Quel est votre mot de passe?")
+ 
+//Tomas
+if (name==="Tomas"){
+  if (password==="Tomas12@{
+      alert('Tu es président')
+}
+}
