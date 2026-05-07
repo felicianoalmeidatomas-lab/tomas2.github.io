0dbaba8b3d84463281b0d6e1de36910baee4c6a1
Tomas <felicianoalmeidatomas@gmail.com>
2026-05-07 21:42:26 +0200

    Refactor prompts for better user experience

--- connexion.js
+++ connexion.js
@@ -1,3 +1,4 @@
+
 function connexion() {
   let status = "non-connecté";
 
@@ -30,61 +31,56 @@ function connexion() {
     status = "connecté";
     alert("Authentification réussie");
     alert("Tu es de niveau 5");
+  }
 
-  
-        }
- if (status === "connecté") {
+  if (status === "connecté") {
 
-      let choix = prompt(
-        "Que souhaites-tu faire ?\n1: Voir des rapports de police\n2: Voir les infos d'un utilisateur"
+    let choix = prompt(
+      "Que souhaites-tu faire ? 1: Voir des rapports de police 2: Voir les infos d'un utilisateur"
+    );
+
+    if (choix === "1") {
+
+      let rapp = prompt(
+        "Quel rapport ? 1: La Fin des Families (Niveau 1) 2: Galactus et Avengers (Niveau 4)"
       );
 
-      if (choix === "1") {
-
-        let rapp = prompt(
-          "Quel rapport ?\n1: La Fin des Families (Niveau 1)\n2: Galactus et Avengers (Niveau 4)"
-        );
-
-        if (rapp === "1") {
-          alert("Le président de Fantasia s'est infiltré dans son ancien gang nommé Families, braqué une banque et placé l'argent dans leur QG. La police a intercepté l'argent et le tribunal a laissé 2 choix à la chef du gang : abandonner le gang ou aller en prison à vie. Elle a choisi d'abandonner le gang.");
-        }
-
-        else if (rapp === "2") {
-          alert("Le chef des Avengers a fait apparaître un Galactus pour évaluer la menace. Galactus a détruit des bâtiments du pays. Ils ont réussi à le tuer mais après de grandes destructions.");
-        }
+      if (rapp === "1") {
+        alert("Le président de Fantasia s'est infiltré dans son ancien gang nommé Families, braqué une banque et placé l'argent dans leur QG. La police a intercepté l'argent et le tribunal a laissé 2 choix à la chef du gang : abandonner le gang ou aller en prison à vie. Elle a choisi d'abandonner le gang.");
       }
 
-      else if (choix === "2") {
+      else if (rapp === "2") {
+        alert("Le chef des Avengers a fait apparaître un Galactus pour évaluer la menace. Galactus a détruit des bâtiments du pays. Ils ont réussi à le tuer mais après de grandes destructions.");
+      }
+    }
 
-        let user = prompt("Quel utilisateur ?\n001\n002\n003\n004\n005");
+    else if (choix === "2") {
 
-        if (user === "001") {
-          alert("Il s'appelle Tomas, est président, ingénieur, n'est pas recherché, n'a pas de casier et a une maison avec assurance.");
-        }
+      let user = prompt("Quel utilisateur ? 001 002 003 004 005");
 
-        else if (user === "002") {
-          alert("Il s'appelle Maxime, est vice-président, ingénieur, n'est pas recherché et a une maison sans assurance.");
-        }
+      if (user === "001") {
+        alert("Il s'appelle Tomas, est président, ingénieur, n'est pas recherché, n'a pas de casier et a une maison avec assurance.");
+      }
 
-        else if (user === "003") {
-          alert("Utilisateur 003 : données non disponibles.");
-        }
+      else if (user === "002") {
+        alert("Il s'appelle Maxime, est vice-président, ingénieur, n'est pas recherché et a une maison sans assurance.");
+      }
 
-        else if (user === "004") {
-          alert("Utilisateur 004 : données non disponibles.");
-        }
+      else if (user === "003") {
+        alert("Utilisateur 003 : données non disponibles.");
+      }
 
-        else if (user === "005") {
-          alert("Utilisateur 005 : données non disponibles.");
-        }
+      else if (user === "004") {
+        alert("Utilisateur 004 : données non disponibles.");
+      }
 
+      else if (user === "005") {
+        alert("Utilisateur 005 : données non disponibles.");
+      }
+    }
+  }
 
-
-
-
+  else {
+    alert("Identifiant ou mot de passe incorrect");
+  }
 }
- }
-    
-  
-
-  
